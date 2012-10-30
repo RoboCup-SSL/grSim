f42ac5b50aa2fd4797b30c18de736c156e900593
Jan Segre <jan@segre.in>
2012-10-30 11:20:22 -0200

    Adding deploy script, use make deploy.

--- Makefile
+++ Makefile
@@ -4,9 +4,16 @@ BUILDDIR=build
 BUILDTYPE=Release
 #BUILDTYPE=Debug
 
-.PHONY: all build mkbuilddir cmake package deb install clean clean-all
+.PHONY: all build mkbuilddir cmake dist package deb install clean clean-all
 
 all: build
+
+deploy: dist upload
+
+# requires pygithub3:
+# pip install pygithub3
+upload:
+	./upload.py --all
 
 build: mkbuilddir cmake
 	$(MAKE) -C $(BUILDDIR)
@@ -16,6 +23,8 @@ mkbuilddir:
 
 cmake: CMakeLists.txt
 	cd $(BUILDDIR) && cmake -DCMAKE_BUILD_TYPE=$(BUILDTYPE) ..
+
+dist: package
 
 package: all
 	$(MAKE) -C $(BUILDDIR) package
---
+++ upload.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+from getpass import getpass
+from os.path import getsize, isfile, join, dirname
+from os import listdir
+from sys import argv
+
+# requires pygithub3:
+# pip install pygithub3
+from pygithub3 import Github
+
+USER = 'roboime'
+REPO = 'grSim'
+GH = None
+
+
+def login():
+    """Authenticates."""
+    login = raw_input('Login: ')
+    passw = getpass()
+    gh = Github(login=login, password=passw, user=USER, repo=REPO)
+    #TODO: check login
+    return gh
+
+
+def deploy(filepath):
+    """Deploys file under filepath."""
+    filename = filepath.split('\\')[-1].split('/')[-1]
+    filesize = getsize(filepath)
+    desc = raw_input('Description: ')
+    print 'Creating resource...'
+    download = GH.repos.downloads.create(dict(name=filename, size=filesize, description=desc))
+    print 'Uploading...'
+    download.upload(filepath)
+
+
+def deploy_all(dirpath):
+    """Deploys all files inside dirpath that are not already deployed."""
+    # List all files that are not hidden
+    #TODO: think about how's that gonna work on windows
+    files = [f for f in listdir(dirpath) if f[0] != '.']
+    # Get all downloads
+    downloads = [d.name for d in GH.repos.downloads.list().all()]
+    # Deploy all files that are not in downloads
+    for f in files:
+        if f not in downloads:
+            print 'Deploying {0}...'.format(f)
+            deploy(join(dirpath, f))
+        else:
+            print '{0} is already deployed.'.format(f)
+
+
+def print_usage():
+    print """Usage:
+./upload.py /path/to/file (will deploy a single file)
+./upload.py --all (will deploy everything under ./dist)
+"""
+
+
+if __name__ == '__main__':
+    if len(argv) != 2:
+        print_usage()
+    elif argv[1] == '--all':
+        try:
+            GH = login()
+            dirpath = join(dirname(__file__), 'dist')
+            deploy_all(dirpath)
+            print 'Done.'
+        except KeyboardInterrupt:
+            print '\nExited.'
+    elif isfile(argv[1]):
+        try:
+            GH = login()
+            deploy(argv[1])
+            print 'Done.'
+        except KeyboardInterrupt:
+            print '\nExited.'
+    else:
+        print 'File not found.'
+        print_usage()
+
