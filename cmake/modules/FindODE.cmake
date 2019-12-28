269bdb2e4ae465bde9c22ac4d4e63f2e9249364e
Kenta Sato <tosainu.maple@gmail.com>
2019-12-28 20:16:24 +0900

    Support older version

--- cmake/modules/FindODE.cmake
+++ cmake/modules/FindODE.cmake
@@ -3,7 +3,14 @@ find_package(PkgConfig QUIET)
 foreach(pc_lib ode-double ode)
   pkg_check_modules(pc_ode QUIET "${pc_lib}")
   if(pc_ode_FOUND)
-    pkg_get_variable(pc_ode_precision "${pc_lib}" precision)
+    if(pc_ode_VERSION VERSION_LESS "0.13")
+      if (pc_ode_CFLAGS MATCHES "-D(dSINGLE|dDOUBLE)")
+        set(pc_ode_precision "${CMAKE_MATCH_1}")
+      endif()
+    else()
+      pkg_get_variable(pc_ode_precision "${pc_lib}" precision)
+    endif()
+
     break()
   endif()
 endforeach()
@@ -35,11 +42,21 @@ if(ODE_LIBRARIES AND NOT TARGET ode::ode)
     IMPORTED_LOCATION "${ODE_LIBRARIES}"
     INTERFACE_INCLUDE_DIRECTORIES "${ODE_INCLUDE_DIR}")
 
-  if(ODE_PRECISION STREQUAL "dSINGLE")
-    set_target_properties(ode::ode PROPERTIES
-      INTERFACE_COMPILE_DEFINITIONS "dIDESINGLE")
-  elseif(ODE_PRECISION STREQUAL "dDOUBLE")
-    set_target_properties(ode::ode PROPERTIES
-      INTERFACE_COMPILE_DEFINITIONS "dIDEDOUBLE")
+  if(ODE_PRECISION VERSION_LESS "0.13")
+    if(ODE_PRECISION STREQUAL "dSINGLE")
+      set_target_properties(ode::ode PROPERTIES
+        INTERFACE_COMPILE_DEFINITIONS "dSINGLE")
+    elseif(ODE_PRECISION STREQUAL "dDOUBLE")
+      set_target_properties(ode::ode PROPERTIES
+        INTERFACE_COMPILE_DEFINITIONS "dDOUBLE")
+    endif()
+  else()
+    if(ODE_PRECISION STREQUAL "dSINGLE")
+      set_target_properties(ode::ode PROPERTIES
+        INTERFACE_COMPILE_DEFINITIONS "dIDESINGLE")
+    elseif(ODE_PRECISION STREQUAL "dDOUBLE")
+      set_target_properties(ode::ode PROPERTIES
+        INTERFACE_COMPILE_DEFINITIONS "dIDEDOUBLE")
+    endif()
   endif()
 endif()
