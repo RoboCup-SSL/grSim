2f304e0f79de6f24242563419a213a7ceaa6d722
Michel Schmid <michel.schmid@robotics-erlangen.de>
2024-02-14 04:06:53 +0100

    fix handling of situations where protobuf is not found at all to also build protobuf correctly

--- cmake/modules/FindOrBuildProtobuf.cmake
+++ cmake/modules/FindOrBuildProtobuf.cmake
@@ -1,11 +1,11 @@
 # sanitize environment before find_package, because otherwise it also looks in the directory created for the ExternalProject
 include(EnvHelper)
 sanitize_env()
-find_package(Protobuf REQUIRED)
+find_package(Protobuf 3.3.0)
 restore_env()
 
 # protobuf versions >= 3.21 are incompatible with how the project is setup and cause weird errors
 # so we build protobuf ourselves
-if(Protobuf_VERSION VERSION_GREATER_EQUAL 3.21)
+if(NOT Protobuf_FOUND OR Protobuf_VERSION VERSION_GREATER_EQUAL 3.21)
   include(BuildProtobuf)
 endif()
